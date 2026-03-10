afd281c1d8aef135f806419aef5d68f23777aff1
sahilkrishna123 <sahilkrishna1243@gmail.com>
2026-03-10 19:43:40 +0500

    Initial Commit

---
+++ src/main/java/com/springboot/web_project_spring_boot/WebProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.web_project_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjectSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/web_project_spring_boot/WebProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.web_project_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
